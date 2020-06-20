689cba0bae34f0ed998e5fbef423b9b04f46a483
Thomas Herzog <Tomrule007@gmail.com>
2020-06-20 12:02:30 -0700

    codeSignal/firstNotRepeatingCharacter Complete

---
+++ codeSignal/firstNotRepeatingCharacter/firstNotRepeatingCharacter.js
@@ -0,0 +1,23 @@
+/*
+
+Given a string s consisting of small English letters, 
+find and return the first instance of a non-repeating 
+character in it. If there is no such character, return '_'.
+*/
+
+function firstNotRepeatingCharacter(s) {
+  const characterCount = s.split('').reduce((charMap, char) => {
+    const currentCount = charMap.get(char) || 0;
+    charMap.set(char, currentCount + 1);
+    return charMap;
+  }, new Map());
+
+  for (const [char, count] in characterCount.entries()) {
+    if (count == 1) return char;
+  }
+  return '_';
+}
+
+const s = 'abacabad';
+
+console.log(firstNotRepeatingCharacter(s));
