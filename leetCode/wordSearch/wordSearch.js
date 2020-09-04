2b85a5559b6336b102c988465498b8a9419adc0f
Thomas Herzog <Tomrule007@gmail.com>
2020-09-04 14:45:44 -0700

    leetCode/wordSearch Solution (with console log)

--- leetCode/wordSearch/wordSearch.js
+++ leetCode/wordSearch/wordSearch.js
@@ -20,68 +20,70 @@ const board = [
  * @param {string} word
  * @return {boolean}
  */
-var exist = function (board, word) {
-  let answerFound = false;
-  const visited = new Set();
+
+const exist = (board, word) => {
   const rowLen = board.length;
   const colLen = board[0].length;
+  const answers = [];
+
+  const checkDirections = (board, word, row, col, path = new Set()) => {
+    console.log({ row, col });
+    // Exit if out of bounds
+    if (row < 0 || row >= rowLen || col < 0 || col >= colLen) {
+      console.log('Out of Bounds');
+      return;
+    }
+
+    // Exit if character doesn't match
+    const currentLetter = board[row][col];
+
+    if (currentLetter !== word[path.size]) {
+      console.log("Characters doesn't match", {
+        currentLetter,
+        charToMatch: word[path.size],
+      });
+      return;
+    }
+
+    // Exit if already visited
+    const pathString = `Board[${row}][${col}] = ${currentLetter}`;
+    if (path.has(pathString)) {
+      console.log('Already visited');
+      return;
+    }
+
+    // Current char is valid add to path.
+    console.log('valid Move', { pathString });
+    path.add(pathString);
+    // Set valid answer if end of word and return
+    if (word.length === path.size) {
+      console.log('Answer Found:', path);
+      answers.push(path);
+      return;
+    }
+
+    // Check next direction
+    console.log({ row, col }, 'up');
+    checkDirections(board, word, row - 1, col, new Set(path)); // up
+    console.log({ row, col }, 'down');
+    checkDirections(board, word, row + 1, col, new Set(path)); // down
+    console.log({ row, col }, 'left');
+    checkDirections(board, word, row, col - 1, new Set(path)); // left
+    console.log({ row, col }, 'right');
+    checkDirections(board, word, row, col + 1, new Set(path)); // right
+  };
 
   // Find first character
   for (let row = 0; row < rowLen; row++) {
     for (let col = 0; col < colLen; col++) {
       const currentChar = board[row][col];
       if (currentChar === word[0]) {
-        checkDirections(board, word, row, col, 0);
+        console.log('First Char', { row, col });
+        checkDirections(board, word, row, col);
       }
     }
   }
-
-  function checkDirections(board, word, row, col, currentMatchIndex) {
-    //check visited
-    const currentCell = row + ':' + col;
-    if (visited.has(currentCell)) return;
-
-    visited.add(currentCell);
-
-    if (currentMatchIndex === word.length - 1) {
-      answerFound = true;
-    }
-
-    const nextLetter = word[currentMatchIndex + 1];
-    //check up
-    const upRow = row - 1;
-    if (upRow >= 0) {
-      const nextMatch = board[upRow][col];
-      if (nextLetter === nextMatch) {
-        console.log('up match');
-        checkDirections(board, word, upRow, col, currentMatchIndex + 1);
-      }
-    }
-    //check down
-    const downRow = row + 1;
-    if (downRow < rowLen) {
-      const nextMatch = board[downRow][col];
-      if (nextLetter === board[downRow][col]) {
-        checkDirections(board, word, downRow, col, currentMatchIndex + 1);
-      }
-    }
-    //check left
-    const leftCol = col - 1;
-    if (leftCol >= 0) {
-      if (nextLetter === board[row][leftCol]) {
-        checkDirections(board, word, row, leftCol, currentMatchIndex + 1);
-      }
-    }
-    //check right
-    const rightCol = col + 1;
-    if (rightCol < colLen) {
-      if (nextLetter === board[row][rightCol]) {
-        checkDirections(board, word, row, rightCol, currentMatchIndex + 1);
-      }
-    }
-  }
-
-  return answerFound;
+  console.log(answers);
+  return answers.length > 0;
 };
-
 module.exports = exist;
