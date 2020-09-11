118f1213014461ec09553a48ddcff8937352bf2c
Thomas Herzog <Tomrule007@gmail.com>
2020-09-11 09:38:00 -0700

    leetCode/countGoodTriplets plan

--- leetCode/countGoodTriplets/countGoodTriplets.js
+++ leetCode/countGoodTriplets/countGoodTriplets.js
@@ -5,6 +5,10 @@
  * @param {number} c
  * @return {number}
  */
-var countGoodTriplets = function (arr, a, b, c) {};
+var countGoodTriplets = function (arr, a, b, c) {
+  // build all triplet permeation
+  //filter those that pass the test
+  //return filtered results length;
+};
 
 module.exports = countGoodTriplets;
