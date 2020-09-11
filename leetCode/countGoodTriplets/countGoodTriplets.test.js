41d070425caa11b6ea59f9e645dd8eb7b957b370
Thomas Herzog <Tomrule007@gmail.com>
2020-09-11 09:33:37 -0700

    leetCode/countGoodTriplets setup

---
+++ leetCode/countGoodTriplets/countGoodTriplets.test.js
@@ -0,0 +1,6 @@
+const countGoodTriplets = require('./countGoodTriplets');
+describe('countGoodTriplets', () => {
+  it('exports a function', async () => {
+    expect(typeof countGoodTriplets).toBe('function');
+  });
+});
