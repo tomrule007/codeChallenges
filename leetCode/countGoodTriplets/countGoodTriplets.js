953ecaea92d30680bdfafd608277fd85683746fb
Thomas Herzog <Tomrule007@gmail.com>
2020-09-11 13:30:47 -0700

    leetCode/countGoodTriplets passes sample exmaples

--- leetCode/countGoodTriplets/countGoodTriplets.js
+++ leetCode/countGoodTriplets/countGoodTriplets.js
@@ -1,3 +1,31 @@
+const tripletPermeations = function (arr) {
+  // build all triplet permeation
+  const triplets = [];
+  for (let i = 0; i < arr.length; i++) {
+    const first = [i, arr[i]];
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) continue;
+      const second = [j, arr[j]];
+      for (let k = 0; k < arr.length; k++) {
+        if (i === k || j === k) continue;
+        const third = [k, arr[k]];
+        triplets.push([first, second, third]);
+      }
+    }
+  }
+  return triplets;
+};
+const indexIsOrdered = (i, j, k) => i < j && j < k;
+const isGoodTriplet = (a, b, c) => ([first, second, third]) => {
+  if (
+    Math.abs(first - second) <= a &&
+    Math.abs(second - third) <= b &&
+    Math.abs(first - third) <= c
+  ) {
+    return true;
+  } else return false;
+};
+
 /**
  * @param {number[]} arr
  * @param {number} a
@@ -6,9 +34,16 @@
  * @return {number}
  */
 var countGoodTriplets = function (arr, a, b, c) {
-  // build all triplet permeation
-  //filter those that pass the test
-  //return filtered results length;
+  const isGoodTripletABC = isGoodTriplet(a, b, c);
+
+  const validTriplets = tripletPermeations(arr).filter(
+    ([[i, first], [j, second], [k, third]]) => {
+      return (
+        indexIsOrdered(i, j, k) && isGoodTripletABC([first, second, third])
+      );
+    }
+  );
+  return validTriplets.length;
 };
 
-module.exports = countGoodTriplets;
+module.exports = { countGoodTriplets, tripletPermeations, isGoodTriplet };
--- leetCode/countGoodTriplets/countGoodTriplets.test.js
+++ leetCode/countGoodTriplets/countGoodTriplets.test.js
@@ -1,6 +1,59 @@
-const countGoodTriplets = require('./countGoodTriplets');
-describe('countGoodTriplets', () => {
-  it('exports a function', async () => {
-    expect(typeof countGoodTriplets).toBe('function');
+const {
+  countGoodTriplets,
+  tripletPermeations,
+  isGoodTriplet,
+} = require('./countGoodTriplets');
+describe('countGoodTriplets.js', () => {
+  describe('tripletPermeations', () => {
+    // Lookup nPr Permutations (n <- object count , r <- sample size)
+    // formula: P(n,r)=n!/(n−r)!  (r === 3)
+    it('is a function', async () => {
+      expect(typeof tripletPermeations).toBe('function');
+    });
+
+    it('returns array of length 6 when given array of length 3', async () => {
+      expect(tripletPermeations([1, 2, 3]).length).toBe(6);
+    });
+    it('returns array of length 24  when given array of length 4', async () => {
+      expect(tripletPermeations([1, 2, 3, 4]).length).toBe(24);
+    });
+
+    it('returns empty array when given array of less than three', async () => {
+      expect(tripletPermeations([1]).length).toBe(0);
+      expect(tripletPermeations([1]).length).toBe(0);
+      expect(tripletPermeations([1, 2]).length).toBe(0);
+    });
+  });
+  describe('isGoodTriplet', () => {
+    it('is a function', async () => {
+      expect(typeof isGoodTriplet).toBe('function');
+    });
+    describe('Returns good for all samples', () => {
+      const a = 7;
+      const b = 2;
+      const c = 3;
+      const isGoodTripletABC = isGoodTriplet(a, b, c);
+      const goodTriplets = [[[3, 0, 1]], [[3, 0, 1]], [[3, 1, 1]], [[0, 1, 1]]];
+      test.each(goodTriplets)('given %p, returns true', (goodTriplet) => {
+        expect(isGoodTripletABC(goodTriplet)).toBe(true);
+      });
+    });
+  });
+  describe('countGoodTriplets', () => {
+    it('is a function', async () => {
+      expect(typeof countGoodTriplets).toBe('function');
+    });
+    describe('Passes these examples', () => {
+      // arr,a,b,c,results
+      const examples = [
+        [[3, 0, 1, 1, 9, 7], 7, 2, 3, 4],
+        [[1, 1, 2, 2, 3], 0, 0, 1, 0],
+      ];
+      test.each(examples)(
+        'given arr: %p a: %p b: %p c: %p, returns %p',
+        (arr, a, b, c, results) =>
+          expect(countGoodTriplets(arr, a, b, c)).toBe(results)
+      );
+    });
   });
 });
