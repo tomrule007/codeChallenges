8ca177dc9203865a86e470f89f5839221bc22bb3
Thomas Herzog <Tomrule007@gmail.com>
2020-09-12 09:18:49 -0700

    validParenthesese - plan

--- leetCode/validParenthesese/validParenthesese.js
+++ leetCode/validParenthesese/validParenthesese.js
@@ -2,6 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+
+const isParentheses = (char) =>
+  ['(', ')', '{', '}', '[', ']'].some((valid) => char === valid);
+var isValid = function (s) {
+  // use stack data type to track valid adds (array pop / push / peek arr[length-1])
+  // check char is valid paren (invalid -> RETURN FALSE)
+  // peek stack if valid
+  //    1)closes matching set (pop them off) (decrement open count for type)
+  //    2)closes mismatching set --> RETURN FALSE
+  //    3)undefined and opening --> push on to stack (increment open count for type)
+  //    4)opening against another opening () --> push (increment open count for type)
+  // RETURN TRUE IF IT PASSES ABOVE 4 Tests
+  // easy outs
+  //    odd character count -> RETURN FALSE
+  //    invalid Characters -> RETURN FALSE
+};
 
 module.exports = isValid;
