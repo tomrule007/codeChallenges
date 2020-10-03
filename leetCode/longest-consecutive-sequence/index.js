52b93de3a2c81ebe58bf2947a898c57b62e2b575
Thomas Herzog <Tomrule007@gmail.com>
2020-10-02 21:12:32 -0700

    longest-consecutive-sequence Complete

--- leetCode/longest-consecutive-sequence/index.js
+++ leetCode/longest-consecutive-sequence/index.js
@@ -1,4 +1,38 @@
+/**
+ *
+ * @param {number[]} nums
+ * @returns {number} longest consecutive count
+ * @example [100, 4, 200, 1, 3, 2] -> 4
+ */
 const longestConsecutive = (nums) => {
-  return null;
+  const consecutiveHash = {};
+  let longest = 0;
+
+  nums.forEach((num) => {
+    if (consecutiveHash[num] === undefined) {
+      const current = (consecutiveHash[num] = {
+        top: { num },
+        bottom: { num },
+      });
+
+      // Look up
+      const above = consecutiveHash[num + 1];
+      if (above !== undefined) {
+        above.bottom.num = current.bottom.num;
+        current.bottom = above.bottom;
+        current.top = above.top;
+      }
+
+      // Look down
+      const below = consecutiveHash[num - 1];
+      if (below !== undefined) {
+        below.top = current.top;
+        current.bottom.num = below.bottom.num;
+      }
+
+      longest = Math.max(current.top.num - current.bottom.num + 1, longest);
+    }
+  });
+  return longest;
 };
 module.exports = longestConsecutive;
--- leetCode/longest-consecutive-sequence/testCases.js
+++ leetCode/longest-consecutive-sequence/testCases.js
@@ -1,3 +1,12 @@
 const tableName = 'given %p expected %s';
-const testTable = [[[100, 4, 200, 1, 3, 2], 4]];
+const testTable = [
+  [[100, 4, 200, 1, 3, 2], 4],
+  [[100, 4, 200, 1, 3, 2, 5], 5],
+  [[5, 6, 100, 4, 200, 1, 3, 2, 5], 6],
+  [[1, 2], 2],
+  [[7, 5, 4, 1], 2],
+  [[], 0],
+  [[1], 1],
+  [[2, 2, 2, 2, 2], 1],
+];
 module.exports = { testTable, tableName };
