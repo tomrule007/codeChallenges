4036fbb9aa38dfc4dd977d08b2a537cb24d1a0bc
Thomas Herzog <Tomrule007@gmail.com>
2020-10-03 14:55:56 -0700

    spiral-matrix WIP refactor to pointer method

--- leetCode/spiral-matrix/index.js
+++ leetCode/spiral-matrix/index.js
@@ -4,44 +4,45 @@
  * @param {Number[][]} matrix
  * @returns {Number[]}
  */
+
 const spiralOrder = (matrix) => {
-  const remainingInput = matrix.map((row) => [...row]);
-  const output = [];
-  let sides = ['TOP', 'RIGHT', 'BOTTOM', 'LEFT'];
-  let sidePointer = 0;
-  while (remainingInput.length > 0 && remainingInput[0].length > 0) {
-    let slice = [];
-    switch (sides[sidePointer]) {
-      case 'TOP':
-        slice = remainingInput.shift();
-        break;
+  const out = [];
+  let m = 0;
+  let n = 0;
+  const limit = {
+    left: 0, // n min
+    right: matrix[0].length - 1, // n max
+    up: 0, // m min
+    down: matrix.length - 1, // m max
+  };
 
-      case 'RIGHT':
-        slice = remainingInput.reduce(
-          (selection, row) => [...selection, row.pop()],
-          []
-        );
-        break;
-
-      case 'BOTTOM':
-        remainingInput[remainingInput.length - 1].reverse();
-        slice = remainingInput.pop();
-        break;
-
-      case 'LEFT':
-        slice = remainingInput.reduce(
-          (selection, row) => [...selection, row.shift()],
-          []
-        );
-        slice.reverse();
-        break;
-      default:
-        throw Error('INVALID Side: ', sides[sidePointer]);
+  let debugStop = 0;
+  while (
+    (limit.left <= limit.right || limit.up <= limit.down) &&
+    debugStop++ < 50
+  ) {
+    // Right (n++) to Right Limit (inc up limit)
+    while (n < limit.right) {
+      out.push(matrix[m][n++]);
     }
-    output.push(...slice);
-    sidePointer = (sidePointer + 1) % 4;
+    limit.up++;
+    // Down (m++) to down Limit (dec right limit)
+    while (m < limit.down) {
+      out.push(matrix[m++][n]);
+    }
+    limit.right--;
+    // Left (n--) to Left Limit (dec down Limit)
+    while (n > limit.left) {
+      out.push(matrix[m][n--]);
+    }
+    limit.down--;
+    // Up (m--) to up Limit (inc left limit)
+    while (m > limit.up) {
+      out.push(matrix[m--][n]);
+    }
+    limit.left++;
   }
-  return output;
+  return out;
 };
 
 module.exports = spiralOrder;
