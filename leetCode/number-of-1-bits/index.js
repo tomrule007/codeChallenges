b79af5a89a4ea17c6fb7922190a3207eb3c5c924
Thomas Herzog <Tomrule007@gmail.com>
2020-10-06 15:43:40 -0700

    number-of-1-bits Complete

--- leetCode/number-of-1-bits/index.js
+++ leetCode/number-of-1-bits/index.js
@@ -2,6 +2,15 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-var hammingWeight = function (n) {};
+var hammingWeight = function (n) {
+  // This solution will only work for 32bit numbers 2^32 - 1
+  let counterMask = 1;
+  let totalOnes = 0;
+  do {
+    if (n & counterMask) totalOnes++;
+  } while ((counterMask <<= 1));
+
+  return totalOnes;
+};
 
 module.exports = hammingWeight;
